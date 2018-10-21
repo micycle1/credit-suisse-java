354edce878bcdb84ab81fe04cf8f0c36ff9c5e68
Michael Carleton <micycle1@hotmail.co.uk>
2018-10-21 15:44:05 +0100

    q1 (naive)

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -2,9 +2,15 @@ package answers;
 
 public class Question1 {
 
-	public static int bestMergedPortfolio(int[] portfolios) {
-		// TODO Auto-generated method stub
-		return -1;
-	}
-
+    public static int bestMergedPortfolio(int[] portfolios) {
+        int highest = 0;
+        for (int i : portfolios) {
+            for (int i1 : portfolios) {
+                if ((i ^ i1) > highest) {
+                    highest = i ^ i1;
+                }
+            }
+        }
+        return highest;
+    }
 }
