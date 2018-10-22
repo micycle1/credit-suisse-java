56ec09a610269a3c15b8d02e736c357f49cddd24
Michael Carleton <micycle1@hotmail.co.uk>
2018-10-22 17:41:51 +0100

    fix 4.

--- src/main/java/answers/Question4.java
+++ src/main/java/answers/Question4.java
@@ -3,45 +3,45 @@ package answers;
 public class Question4 {
 
     public static int selectionFailedTradedesks(String[][] rows, int numberMachines) {
-        int minTime =  Integer.MAX_VALUE;
+        int minTime = Integer.MAX_VALUE;
         for (String[] row : rows) {
-
             int first_index = 0;
             int total_sum = 0;
             int count = 0;
 
-            for (int i = 0; i < row.length; i++) {
-                if (row[i].equals("X")) {
-                    if (count == numberMachines) {
-                        minTime = Math.min(minTime, total_sum);
-                    }
-                    first_index = 0;
-                    total_sum = 0;
-                    count = 0;
-                } else {
-                    if (count == 0) {
-                        first_index = i;
-                        total_sum = Integer.valueOf(row[i]);
-                        count++;
-                    }
-                    else {
-                        if (count == numberMachines) {
-                            minTime = Math.min(minTime, total_sum);
-                            total_sum -= Integer.valueOf(row[first_index]);
-                            first_index++;
+            check_char :
+            {
+                for (int i = 0; i < row.length; i++) {
+                    if (row[i].equals("X")) {
+                        if ((row.length - (i+1)) < numberMachines) {
+                            break check_char;
                         }
-                        else {
-                            total_sum += Integer.valueOf(row[i]);
+                        first_index = 0;
+                        total_sum = 0;
+                        count = 0;
+                    } else {
+                        if (count == 0) {
+                            first_index = i;
+                            total_sum = Integer.valueOf(row[i]);
                             count++;
+                        } else {
+                            if (count + 1 == numberMachines) {
+                                total_sum += Integer.valueOf(row[i]);
+                                minTime = Math.min(minTime, total_sum);
+                                total_sum -= Integer.valueOf(row[first_index]);
+                                first_index++;
+                            } else {
+                                total_sum += Integer.valueOf(row[i]);
+                                count++;
+                            }
                         }
                     }
                 }
             }
         }
-
         if (minTime == Integer.MAX_VALUE) {
             minTime = 0;
         }
-        return minTime; // return min here?
+        return minTime;
     }
 }
--- src/main/java/source/Main.java
+++ src/main/java/source/Main.java
@@ -17,12 +17,12 @@ public class Main {
 //		int x = Question1.bestMergedPortfolio(new int[]{15, 8, 6, 7});
 //        int y = Question5.shareExchange(new int[]{1,4,2}, 3);
 //        int z = Question4.selectionFailedTradedesks(new String[][]{
-//                {"12", "12", "3", "X", "3"},
+//                {"12", "12", "3", "4", "3"},
 //                {"23", "X", "X", "X", "3"},
 //                {"33", "21", "X", "X", "X"},
 //                {"9", "12", "3", "X", "X"},
 //                {"X", "X", "X", "4", "5"}
-//        }, 3);
+//        }, 2);
 //        int a = Question4.selectionFailedTradedesks(new String[][]{{"X", "X", "2"}, {"2", "3", "X"}, {"X", "3", "2"}}, 3);
 //        System.out.println(a);
         SpringApplication.run(Main.class, args);
